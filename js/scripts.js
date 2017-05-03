f1b32b0d77d6f02fefd3dc2aaed8abcfcf646af7
Mownisha <reach.mownisha@gmail.com>
2017-05-02 20:27:51 -0700

    added javascript and jquery to my webpage

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("#formOne").submit(funtion(event) {
+    var a=$("input#name").val();
+    var b=$("input#date").val();
+    var c=$("input#food").val();
+    var d=$("input#music").val();
+    var e=$("input#place").val();
+    var f=$("input#color").val();
+    var g=$("input#person").val();
+    var h=$("input#sports").val();
+
+    event.preventDefault();
+  });
+});
